ce913996e3bb7700f74d758883dc1583084446d3
Ritual <dev@theritual.eu>
2021-12-15 08:11:35 +0100

    Created default theme

--- Main/theme/SWATCHES.js
+++ Main/theme/SWATCHES.js
@@ -1,3 +1,9 @@
 const SWATCHES = {
-    cinder: '#0c0c16',
+    cinder: '#0C0C16',
+    denim: '#1248E0',
+    ecruWhite: '#F6F5E9',
+    gold: '#FFD600',
+    flushOrange: '#FF7800'
 }
+
+export default SWATCHES;
---
+++ Main/theme/default.js
@@ -0,0 +1,16 @@
+import SWATCHES from "./SWATCHES";
+
+const primary = SWATCHES.denim;
+const secondary = SWATCHES.flushOrange;
+const accent = SWATCHES.gold;
+const dark = SWATCHES.cinder;
+const light = SWATCHES.ecruWhite;
+
+const theme = {
+    default: {
+        background: dark,
+        text: light,
+    }
+}
+
+export default theme;
