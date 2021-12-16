65c2ae0eeddc7cf52f0d138fe79b104cdbf852ff
Ritual <dev@theritual.eu>
2021-12-16 13:07:28 +0100

    added possibility to chose background file in BasicBackground component

--- Main/common/BasicBackground.js
+++ Main/common/BasicBackground.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ImageBackground } from 'react-native';
 
 const BasicBackground = (props) => {
+    const { file } = props;
+    const uri = file ? file : require('../../assets/images/background.png');
     return (
         <ImageBackground
             {...props}
             style={{ width: '100%', height: '100%', ...props.style }}
-            source={require('../../assets/images/background.png')}
+            source={uri}
             resizeMode='cover'>
             {props.children}
         </ImageBackground>
